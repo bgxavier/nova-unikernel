b53accbc29cce12bd56b0bbc67dd607535fd3928
Bruno Xavier <bgxavier@gmail.com>
2015-12-15 22:38:05 -0200

    vai

--- novaunikernel/virt/unikernel/driver.py
+++ novaunikernel/virt/unikernel/driver.py
@@ -4,6 +4,7 @@ import os
 
 from oslo_log import log
 from oslo_config import cfg
+from git import exc
 
 from nova.image import glance
 from nova.virt.libvirt import driver as libvirt_driver
@@ -15,16 +16,14 @@ environ = os.environ.copy()
 unikernel_opts = [
     cfg.StrOpt('branch',
                default='master',
-               help='branch to be fetched'),
-    cfg.StrOpt('repo_path',
+               help='branch'),
+    cfg.StrOpt('repo_base',
                default='/opt/stack/data/unikernel',
-               help='branch to be fetched'),
+               help='unikernels repo path'),
 ]
 
 CONF = cfg.CONF
 CONF.register_opts(unikernel_opts, 'unikernel')
-
-build_path = os.path.join(CONF.unikernel.repo_path, 'unikernel/unikernel.qemu')
 
 LOG = log.getLogger(__name__)
 
@@ -48,7 +47,8 @@ class UnikernelDriver(libvirt_driver.LibvirtDriver):
         self.image_fetch(instance,
                          filename,
                          repo_url,
-                         CONF.unikernel.repo_path,
+                         image_id,
+                         CONF.unikernel.repo_base,
                          CONF.unikernel.branch)
 
         image.cache(fetch_func=fetch_func,
@@ -59,21 +59,28 @@ class UnikernelDriver(libvirt_driver.LibvirtDriver):
                     project_id=instance.project_id,
                     size=size)
 
-    def image_fetch(self, instance, filename, repo_url, repo_path, branch):
+    def image_fetch(self, instance, filename, repo_url, image_id, repo_base,
+                    branch):
 
-        repo = Repo.init(repo_path)
+        # initialize the unikernel repository
+        unikernel_repo = os.path.join(repo_base, image_id)
+        repo = Repo.init(unikernel_repo)
 
         LOG.debug("Fetching repo %s...", repo_url)
+
         try:
             origin = repo.create_remote('origin', repo_url)
         except:
             origin = repo.remotes['origin']
+
         try:
             origin.fetch(branch)
-        except:
+        except exc.GitCommandError:
+            raise
+
         origin.pull(origin.refs[branch].remote_head)
 
-        self.compile_image(repo_path)
+        self.compile_image(repo_base, unikernel_repo, image_id)
 
         # Convert from qcow2 to raw
         instance_path = os.path.join(CONF.instances_path, '_base')
@@ -83,14 +90,16 @@ class UnikernelDriver(libvirt_driver.LibvirtDriver):
         if os.path.exists(staged_path):
             os.unlink(staged_path)
 
-        images.convert_image(build_path, staged_path, 'raw')
+        compiled_image = os.path.join(unikernel_repo,
+                                      image_id + '.qemu')
+        images.convert_image(compiled_image, staged_path, 'raw')
 
     def check_branch_diffs(self, repo, branch):
         return repo.git.diff("origin/%s" % branch)
 
-    def compile_image(self, repo_path):
+    def compile_image(self, repo_base, unikernel_repo,  image_id):
         LOG.debug("Recompiling image ...")
-        p = subprocess.Popen(['capstan', 'build'],
-                             cwd=repo_path,
-                             env=dict(environ, CAPSTAN_ROOT=repo_path))
+        p = subprocess.Popen(['capstan', 'build', image_id],
+                             cwd=unikernel_repo,
+                             env=dict(environ, CAPSTAN_ROOT=repo_base))
         p.wait()
